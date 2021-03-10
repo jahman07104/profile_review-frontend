fcc34b5db9ad225dd168e80784cb59048f3bedcc
jahman07104 <jahman@bellatlantic.net>
2021-03-10 00:06:07 -0500

    cleanup profile and app components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './css/App.css';
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from './components/Home';
-import Profile from './components/Profile';
-import AboutThisApp from './components/AboutThisApp';
-import Instructions from './components/Instructions';
+import React from "react";
+import "./css/App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import AboutThisApp from "./components/AboutThisApp";
+import Instructions from "./components/Instructions";
 import store from "./store";
 import { Provider } from "react-redux";
-import PageNotFound from './components/PageNotFound';
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/profile/:id" component={Profile} />          
-          <Route exact path="/about-this-app" component={AboutThisApp} />
-          <Route exact path="/instructions" component={Instructions} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/profile/:id' component={Profile} />
+          <Route exact path='/about-this-app' component={AboutThisApp} />
+          <Route exact path='/instructions' component={Instructions} />
           <Route component={PageNotFound} />
         </Switch>
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,48 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './Navbar'
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
-import Footer from'./Footer'
-import ProfileCard from './ProfileCard';
+import Footer from "./Footer";
+import ProfileCard from "./ProfileCard";
 
-const About = () => {
-  const { id } = useParams();  
+const Profile = () => {
+  const { id } = useParams();
   const [profile, setProfile] = useState({});
 
   const getProfile = () => {
     fetch(`http://localhost:4000/profiles/${id}`)
-    .then((res) => res.json())
-    .then(profile => {
-      setProfile(profile);
-    })    
-  }
+      .then((res) => res.json())
+      .then((profile) => {
+        setProfile(profile);
+      });
+  };
 
   useEffect(() => {
     getProfile();
   });
 
-  return ( 
+  return (
     <>
-      <div className="container">
-        <Navbar title="Profile viewer" icon="fab fa-forumbee"/>  
-          <h1>  About Me </h1>
-        <div className="row">
-          <div className=" col-md-4 col-md-offset-.6">
-          </div>
-          <ProfileCard id={profile.id}
-          name={profile.name} 
-          email={profile.email} 
-          city={profile.city} 
-          phone={profile.phone} 
-          avatar={profile.image}
-          rating={profile.rating} />
+      <div className='container'>
+        <Navbar title='Profile viewer' icon='fab fa-forumbee' />
+        <h1> About Me </h1>
+        <div className='row'>
+          <div className=' col-md-4 col-md-offset-.6'></div>
+          <ProfileCard
+            id={profile.id}
+            name={profile.name}
+            email={profile.email}
+            city={profile.city}
+            phone={profile.phone}
+            avatar={profile.image}
+            rating={profile.rating}
+          />
         </div>
         <br></br>
-        <div style={{ borderTop: "2px solid #fff ", marginLeft: 2, marginRight: 2 }}>
-        </div>
-        <Footer year={new Date().getFullYear()}/>
+        <div
+          style={{
+            borderTop: "2px solid #fff ",
+            marginLeft: 2,
+            marginRight: 2,
+          }}
+        ></div>
+        <Footer year={new Date().getFullYear()} />
       </div>
     </>
-);
-
-}
-export default About;
+  );
+};
+export default Profile;
